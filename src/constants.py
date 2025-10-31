e6211f42f79e50eb7201c372fbdd76331353d95a
RichardHolzhofer <rchrdhlzhfr@gmail.com>
2025-10-31 16:24:08 +0100

    Data extraction and transformation completed

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
 POSTGRES_CONN_ID = "crypto_db"
 POSTGRES_BITCOIN_TABLE_NAME = "bitcoin_price"
 POSTGRES_ETHEREUM_TABLE_NAME = "ethereum_price"
+
+COINGECKO_BASE_URL = "https://api.coingecko.com/api/v3/coins/"
